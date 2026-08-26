611d6f73181ce50b99eef674748e03caed29f4bf
Capitão Assumção <assumcao@gmail.com>
2026-08-26 09:15:03 -0300

    feat: register TOTP MFA provider

--- backend/src/account/account.module.ts
+++ backend/src/account/account.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
+import { TotpService } from './totp.service';
 
-@Module({ controllers: [AccountController], providers: [AccountService] })
+@Module({ controllers: [AccountController], providers: [AccountService, TotpService] })
 export class AccountModule {}
