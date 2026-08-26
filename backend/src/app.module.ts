12674301b86e0107c0c25cd46b22af6f341b7309
Capitão Assumção <assumcao@gmail.com>
2026-08-26 08:18:02 -0300

    feat: register organizations and professionals modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { HealthModule } from './health/health.module';
+import { OrganizationsModule } from './organizations/organizations.module';
 import { PetsModule } from './pets/pets.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProfessionalsModule } from './professionals/professionals.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, HealthModule, UsersModule, AuthModule, PetsModule],
+  imports: [
+    PrismaModule,
+    HealthModule,
+    UsersModule,
+    AuthModule,
+    PetsModule,
+    OrganizationsModule,
+    ProfessionalsModule,
+  ],
 })
 export class AppModule {}
